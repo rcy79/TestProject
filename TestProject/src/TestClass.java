810a5f0edfc1796322c6076190f6a6a7fead73d0
yarra <yarra@192.168.1.111>
2018-06-13 21:33:22 -0500

    new print statement. .

--- TestProject/src/TestClass.java
+++ TestProject/src/TestClass.java
@@ -4,6 +4,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Printing test class. .");
 		System.out.println("Print. . .");
+		System.out.println("Happy. . ");
 
 	}
 
